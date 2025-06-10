9f0b9d2af00896f9cf2846f75dbc4ef59e512d12
Kamoosh Shaik <kamooshbaba786@gmail.com>
2025-06-10 14:09:36 +0530

    Add files via upload

---
+++ 2.variables.py
@@ -0,0 +1,24 @@
+#Variable = A container for a value (string, integer, float, boolean)
+#           Variables behaves as if it was the value it contains
+
+# String - a series of characters
+first_name = "Kamoosh"
+print(f"Hello {first_name}")
+
+# integer - whole number
+
+age = 25  # integer should not be within quotes
+print(f"you are  {age} years old.")
+
+# float - its a number contains a decimal portion.
+
+price = 10.99
+
+# boolean - True or False
+
+if_student = True
+
+if if_student:
+    print("You are a Student")
+else:
+    print("You are not a Student")
