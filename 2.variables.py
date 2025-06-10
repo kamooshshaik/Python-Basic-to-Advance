f76641ba2b7a9e70be11315f6eb05a255e35864d
Kamoosh Shaik <kamooshbaba786@gmail.com>
2025-06-10 16:24:29 +0530

    Update 2.variables.py

--- 2.variables.py
+++ 2.variables.py
@@ -1,24 +1,52 @@
-#Variable = A container for a value (string, integer, float, boolean)
-#           Variables behaves as if it was the value it contains
+# Variable = A container for a value ( string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
 
-# String - a series of characters
-first_name = "Kamoosh"
-print(f"Hello {first_name}")
+# Strings = a string is a series of characters they can include the numbers but we treat its as characters
+first_name = "Bro"
+food = "gulabjamun"
+email = "kamooshbaba786@gmail.com"
 
-# integer - whole number
+print(first_name)
+print(f"Hello {first_name}") # to add extra comment to our output
+print(f"i like {food}")
+print(f"my email is {email}")
 
-age = 25  # integer should not be within quotes
-print(f"you are  {age} years old.")
+# integer = a integer is a whole numbers, a integer should not be in quots "" because its a integer techenically.
+#           another example is interger can be a quantity and amount of people.
+age = 25
+quantity = 8
+no_of_students = 60
 
-# float - its a number contains a decimal portion.
+print(f"you are {age} years old!")
+print(f"i am buying {quantity} apples!")
+print (f"in my class has {no_of_students} students. ")
 
+
+# Float = a float is a number but it contains a decimal portion.
 price = 10.99
+gpa = 8.03
+distance = 10.87
+print(f"the price of a chocklate is ${price}.")
+print(f"my gpa is {gpa}.")
+print(f"Distance from my home to college is {distance}km")
 
-# boolean - True or False
 
-if_student = True
+# boolean = a boolean is either True of False (1st letter should be capital).
 
-if if_student:
-    print("You are a Student")
+is_student = True
+graduation = False
+
+print(f"are you a student? : {is_student}")
+print(f"are you completed you graduation? : {graduation}")
+
+
+if is_student:
+    print("You are a student")
 else:
-    print("You are not a Student")
+    print("you are not a student")
+    
+    
+if graduation:
+    print("You are completed your graduation")
+else:
+    print("you are not completed your graduation")
