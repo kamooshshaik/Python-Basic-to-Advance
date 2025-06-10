301032c81cf249a184a5e8b38dd9906ce91b9e43
Kamoosh Shaik <kamooshbaba786@gmail.com>
2025-06-10 16:26:20 +0530

    Update 4.User_input.py

--- 4.User_input.py
+++ 4.User_input.py
@@ -1,29 +1,29 @@
-# input() = A function that prompts the user to enter the data 
-#           Returns  the entered data as a string
+# Input =  A function that prompts the user to enter data 
+#          Returns the entered data as a string
 
-name = input("Enter Your name : ")
-age = int(input("Enter your age: "))
+
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+age = age + 1
 
 print(f"Hello {name}")
-age = age + 1   # This is only possible if the age is in integer form.
-print(f"Your {age} years old")
+print(f"You are {age} years old.")
 
 
-# Excercise - 1 -> Rectangular area calculation
-length = float(input("Enter a length of a rectangular:" ))
-width = float(input("Enter width of a rectangular: "))
-
+# Excercise - 1 (Calculation of Area of rectangle)
+length = float(input("Enter length of rectangle (l) in meters : "))
+width = float(input("Enter width of Rectangle (b) in meters : "))
 area = length * width
 
-print(f"Area of a rectangular is {area}m².") # for square symbol turn number lock + alt +0178.
-
-# Excercise - 2 -> Shopping cart Programm
-
-item = input("what would you like to buy: ")
-price = float(input("Enter the price of the product: "))
-quantity = int(input("Enter no of products: "))
-
-print(f"You have brought total of {item} x {quantity}/s")
-print(f"You need to pay the amount of Rs.{price * quantity}/-.")
+print(f"Area of a Rectangle is {area}m².") # for square symbol (Alt + 0178) and (CTRL + /) for select and make comment.
 
 
+# Excercise - 2 (Shopping cart program)
+item = input("What item would you like to buy?: ")
+price = float(input("What was the price?: "))
+quantity = int(input("How many would you like to buy?: "))
+
+total = price * quantity
+
+print(f"You are brought a {quantity} x {item}/sPizza"  )
+print(f"you are buying a {item}s at an total cost of Rs.{total}.")
