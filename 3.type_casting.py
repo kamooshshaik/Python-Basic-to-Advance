afcf28f6b256bfb33d0e626a49dbde3afb76913d
Kamoosh Shaik <kamooshbaba786@gmail.com>
2025-06-10 16:25:07 +0530

    Update 3.type_casting.py

--- 3.type_casting.py
+++ 3.type_casting.py
@@ -1,22 +1,32 @@
-#Type Casting = the process of converting a variable from one data type to another
-#                str(), float() , bool(), int()
+#Typecasting = the process of converting a variable from one data type to another data type
+#              str(), int(), float(), bool()
 
-name = "Shaik Kamoosh"
+name = "Kamoosh"
 age = 25
-cpi = 8.3
-is_student = True
+cpi = 8.03
+student = True
 
+# Checking data type
 print(type(name))
 print(type(age))
 print(type(cpi))
-print(type(is_student))
+print(type(student))
 
-
-# Type cast
-# float to -> int
+# Converting one data type to another
+# converting float to int.
 cpi = int(cpi)
+print(type(cpi))
 print(cpi)
 
-# int to -> float
-age = float(age)
+# converting int to str.
+age = str(age)
+print(type(age))
+
 print(age)
+# adding 1 at the end of 25 it become 251 because its cnverted to string.
+age += "1"
+print(age)
+
+# converting str to bool.
+name = bool(name)
+print(name) # output will be true , if string is an empty " " output will be false.
